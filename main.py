90504584b06d8faf1ab6c85d1e864c2fb5d8ae0d
Kacper <wujekalbercik@gmail.com>
2022-11-05 14:26:11 +0100

    add printouts

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import random
-input("What number from 0 to 100 I am thinking about? ")
-x = int(random.randrange(0 , 100))
+userpick = input("What number from 0 to 100 I am thinking about? ")
+picked = int(random.randrange(0 , 100))
+print(f"userpick: {userpick}")
+print(f"picked: {picked}")
+
+
+input("Thanks for the game")
