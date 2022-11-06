67e0baacf41212396b9b16cbed8ad18d0e2888c0
Kacper <wujekalbercik@gmail.com>
2022-11-06 13:20:37 +0100

    made loop works

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ userpick = input("What number from 0 to 100 I am thinking about? ")
 picked = int(random.randrange(0 , 100))
 print(f"userpick: {userpick}")
 print(f"picked: {picked}")
-
-
-input("Thanks for the game")
+while userpick != picked:
+    picked = int(random.randrange(0 , 100))
+    userpick = input("Not this one. Try another one ")
+    print(f"userpick: {userpick}")
+    print(f"picked: {picked}")
+else:
+    name = str(input("You made it! Let`s save your score. Give me your nick: "))
