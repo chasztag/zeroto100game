2e51fb78a4c606e64d12e989d2e3a9ff27e0f34e
Kacper <wujekalbercik@gmail.com>
2022-11-06 15:58:54 +0100

    loop get counted

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ userpick = input("What number from 0 to 100 I am thinking about? ")
 picked = int(random.randrange(0 , 100))
 print(f"userpick: {userpick}")
 print(f"picked: {picked}")
+number_of_tries = 1
 while userpick != picked:
     picked = int(random.randrange(0 , 100))
     userpick = input("Not this one. Try another one ")
     print(f"userpick: {userpick}")
     print(f"picked: {picked}")
+    number_of_tries += 1
 else:
     name = str(input("You made it! Let`s save your score. Give me your nick: "))
+    print(f"You tried {number_of_tries} times")
